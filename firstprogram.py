a2e42ad3ccb7d4421ab89b4c9719391e286f71b7
Sapdotten <semenova.a.p.04@gmail.com>
2022-11-22 21:38:37 +0400

    убрала задержку по времени

--- firstprogram.py
+++ firstprogram.py
@@ -4,9 +4,7 @@ import subprocess
 
 class SystemCommand:
 	@classmethod
-	def off(cls, tttime=0):
-		if type(tttime) is not int:
-			raise TypeError("Аргумент должен быть целым числом")
+	def off(cls):
 		_answ = -1
 		while _answ!=0 and _answ!=1:
 			_answ = int(input("Вы точно хотите отключить компьютер? (1 - да, 0 - нет)"))
@@ -14,14 +12,12 @@ class SystemCommand:
 				print("Неизвестная комнада")
 		
 		if _answ==1:
-			time.sleep(tttime)
+
 			os.system('systemctl poweroff') 
 		else:
 			print("Отмена")
 	@classmethod
-	def reboot(cls, tttime=0):
-		if type(tttime) is not int:
-			raise TypeError("Аргумент должен быть целым числом")
+	def reboot(cls):
 		_answ = -1
 		while _answ!=0 and _answ!=1:
 			_answ = int(input("Вы точно хотите перезагрузить компьютер? (1 - да, 0 - нет)"))
@@ -30,12 +26,11 @@ class SystemCommand:
 				print("Неизвестная комнада")
 		
 		if _answ==1:
-			time.sleep(tttime)
 			subprocess.check_call('reboot')
 		else:
 			print("Отмена")
 		
-SystemCommand.reboot(10)
-SystemCommand.off(10)
+SystemCommand.reboot()
+SystemCommand.off()
 
 
