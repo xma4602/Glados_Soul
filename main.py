1d49163327f4dd5ba0fe010bd6e4aeee027e8b91
thanatalice <67954166+testmapper-official@users.noreply.github.com>
2022-11-25 17:33:30 +0400

    №1

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import cv2
+from cvzone.PoseModule import PoseDetector
+from cvzone.FaceDetectionModule import FaceDetector
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Initialization
+    cap = cv2.VideoCapture(0)
+    poseDetector = PoseDetector()
+    faceDetector = FaceDetector()
+
+    while True:
+        # Retrieve the image data till success
+        success, img = cap.read()
+        if not success:
+            continue
+
+        # Find poses & faces
+        img = poseDetector.findPose(img)
+        img, bboxs = faceDetector.findFaces(img)
+
+        # Draw detected faces
+        if bboxs:
+            # bboxInfo - "id","bbox","score","center"
+            center = bboxs[0]["center"]
+            cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+        # Draw detected poses & body particles
+        lmList, bboxInfo = poseDetector.findPosition(img, bboxWithHands=False)
+        if bboxInfo:
+            center = bboxInfo["center"]
+            cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+        # Show the result
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Fin
+    cap.release()
+    cv2.destroyAllWindows()
