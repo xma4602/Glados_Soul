f82334a82014c7ec36f0cbd019361aafe9c7942a
Sapdotten <semenova.a.p.04@gmail.com>
2022-11-29 11:39:20 +0400

    Изменение проверки аргументов методов

--- firstprogram.py
+++ firstprogram.py
@@ -2,35 +2,31 @@ import os
 import time
 import subprocess
 
+
 class SystemCommand:
 	@classmethod
 	def off(cls):
-		_answ = -1
-		while _answ!=0 and _answ!=1:
-			_answ = int(input("Вы точно хотите отключить компьютер? (1 - да, 0 - нет)"))
-			if _answ!=0 and _answ!=1:
+		ans = -1
+		while ans != 'n' and ans != 'y':
+			ans = input("Вы точно хотите отключить компьютер? (y - да, n - нет)")
+			if ans != 'n' and ans != 'y':
 				print("Неизвестная комнада")
-		
-		if _answ==1:
-
-			os.system('systemctl poweroff') 
+		if ans == 'y':
+			os.system('systemctl poweroff')
 		else:
 			print("Отмена")
 	@classmethod
 	def reboot(cls):
-		_answ = -1
-		while _answ!=0 and _answ!=1:
-			_answ = int(input("Вы точно хотите перезагрузить компьютер? (1 - да, 0 - нет)"))
-			print(_answ)
-			if _answ!=0 and _answ!=1:
+		ans = -1
+		while ans != 'n' and ans != 'y':
+			ans = input("Вы точно хотите перезагрузить компьютер? (y - да, n - нет)")
+			print(ans)
+			if ans != 'n' and ans != 'y':
 				print("Неизвестная комнада")
-		
-		if _answ==1:
+		if ans == 'y':
 			subprocess.check_call('reboot')
 		else:
 			print("Отмена")
-		
+
 SystemCommand.reboot()
 SystemCommand.off()
-
-
