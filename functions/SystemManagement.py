82912287cea1ed4467efa070e0b4c15310c5ba26
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-02-14 19:31:25 +0400

    Изменила возвращаемые значения функции модуля SystemManagement.py

--- functions/SystemManagement.py
+++ functions/SystemManagement.py
@@ -3,9 +3,9 @@ import subprocess
 def SystemManagement(command = None):
     if command == "off":
         os.system('systemctl poweroff')  # os.system('shutdown /p /f') на винде
-        return 'success'
+        return 'Завершение работы'
     elif command == "reboot":
         subprocess.check_call('reboot')  # os.system('shutdown -r -t 0') на винде
-        return 'success'
+        return 'Перезагрузка'
     else:
-        return 'unknown command'
+        return 'Неизвестная команда'
