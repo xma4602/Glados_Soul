6e43f6bc428537cf275507340d097e44ddbcaa9a
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-02-14 19:29:54 +0400

    В модуле FuntctionManagement.py изменила проверку команды и возвращаемое значение функции Function

--- FunctionManagement.py
+++ FunctionManagement.py
@@ -5,17 +5,15 @@ Commands = ('system power off', 'system reboot',)
 
 #функция, проверяющая наличие полученной команды в списке команд
 def __CheckCommand(command=None):
-    global Commands
     for keyPhrase in Commands:
-        if all(word in command.split() for word in keyPhrase.split()):
+        if keyPhrase in command:
             return True
     return False
 def Function(command = None):
     if not __CheckCommand(command):
-        return 0
+        return 'Неизвестная команда'
     if command == 'system power off':
         return(SystemManagement.SystemManagement('off'))
     elif command == 'system reboot':
         return(SystemManagement.SystemManagement('reboot'))
-
 
