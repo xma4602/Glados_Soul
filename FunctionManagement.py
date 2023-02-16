f921b4f5c929acb42b5c27ea6c558f554915a1c0
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-02-16 15:42:24 +0400

    Добавила комментарии в файле FunctionManagement.py

--- FunctionManagement.py
+++ FunctionManagement.py
@@ -3,17 +3,27 @@ import functions.SystemManagement as SystemManagement
 #список команд
 Commands = ('system power off', 'system reboot',)
 
-#функция, проверяющая наличие полученной команды в списке команд
+
 def __CheckCommand(command=None):
+    """
+    Проверяет наличие заданной команды в списке команд
+    :param command: заданная команда
+    """
     for keyPhrase in Commands:
         if keyPhrase in command:
             return True
     return False
+
+
 def Function(command = None):
+    """
+    Вызывает заданную команду
+    :param command: заданная команда
+    :return: результат выполнения
+    """
     if not __CheckCommand(command):
         return 'Неизвестная команда'
     if command == 'system power off':
         return(SystemManagement.SystemManagement('off'))
     elif command == 'system reboot':
         return(SystemManagement.SystemManagement('reboot'))
-
