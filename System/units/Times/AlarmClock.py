08b957692ada0081e470e0af8ff6d362ddc33ba2
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-03-02 19:08:35 +0400

    добавлен класс TimeEvent и Alarm

--- System/units/Times/AlarmClock.py
+++ System/units/Times/AlarmClock.py
@@ -1,4 +1,32 @@
 from datetime import datetime
+from TimeEvent import TimeEvent
+import enum
 
-class Alarm(datetime):
-    pass
+
+class RegularDay(enum.Enum):
+    never = -1
+    everyday = 0
+    monday = 1
+    tuesday = 2
+    wednesday = 3
+    thursday = 4
+    friday = 5
+    saturday = 6
+    sunday = 7
+
+class Alarm(TimeEvent):
+    _regular = RegularDay.never
+    _note = "Будильник" #заголвок или подпись к будильнику
+    def SetNote(self, note: str):
+        '''
+        Задает будильнику заголовок
+        :param note: заголвок
+        '''
+        _note = str
+
+
+    def SetRegularMode(self, day = -1):
+        _regular = day
+
+
+
--- System/units/Times/TimeEvent.py
+++ System/units/Times/TimeEvent.py
@@ -1,3 +1,9 @@
-class TimeEvent:
+from datetime import datetime
 
-    pass
+
+class TimeEvent:
+    timedata = datetime()
+    def __init__(self, _year=0, _month=0, _day=0, _hour=0, _minute=0, _second=0, _microsecond=0):
+        timedata = datetime(_year, _month, _day, _hour, _minute, _second, _microsecond)
+
+
