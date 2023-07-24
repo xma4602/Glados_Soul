e654acc245b4e43d2e60a51cf8b6eec18f8b727a
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-07-25 01:25:45 +0400

    Добавила распарсивание конфигов для логов, комментарии к некоторым методам Logger

--- System/modules/logger.py
+++ System/modules/logger.py
@@ -2,47 +2,52 @@ import logging
 from logging import INFO, WARNING
 import json
 import sys
+from typing import List
 
-mess_in_file = True
-
-_log_folder = 'System\\files\\log\\'
 _std_format = '%(asctime)s - %(levelname)s - %(message)s'
-message_file_info = _log_folder + 'message_info.log'
-message_file_warning = _log_folder + 'message_warning.log'
-notice_file = _log_folder + 'notice.log'
+log_out = None  # вывод логов: 'file' или 'console'
+message_file_info = None
+message_file_warning = None
+notice_file = None  # пути к файлам логов
 
 global message
 
 
 class Logger:
     def __init__(self, log_name: str, level):
-        self._logger = logging.getLogger(log_name)
-        self._log_name = log_name
-        self._logger.setLevel(level)
+        self.logger = logging.getLogger(log_name)  # создание логгера
+        self.logger.setLevel(level)  # задание минимального уровня логирования
 
-    def add_handlers(self, handlers: list):
-        for elem in handlers:
-            self._logger.addHandler(elem)
+    def set_formatters(self, format_: str) -> None:
+        """
+        Устанавливает для всех хэндлеров логгера формат записи
+        """
+        formatter = logging.Formatter(format_)  # создание объекта формата
+        for elem in self.logger.handlers:
+            elem.setFormatter(formatter)  # установка формата для каждого хэндлера
 
-    def set_formatters(self, format_: str):
-        formatter = logging.Formatter(format_)
-        for elem in self._logger.handlers:
-            elem.setFormatter(formatter)
-
-    def add_file_handler(self, file: str, level, format_=_std_format):
+    def add_file_handler(self, file: str, level: int, format_: str = _std_format) -> None:
+        """Добавляет в логгер хэндлер для записи логов в файл
+        :param file: путь к файлу для записи логов
+        :param level: уровень минимального логирования хэндлера
+        :param format_: формат записи логов, по умолчанию _std_format"""
         message_handler = logging.FileHandler(file)
         message_handler.setLevel(level)
         message_handler.setFormatter(logging.Formatter(format_))
-        self._logger.addHandler(message_handler)
+        self.logger.addHandler(message_handler)
 
     def add_console_handler(self, level, format_=_std_format, stream=sys.stdout):
+        """Добавляет в логгер хэндлер для записи логов в консоль
+        :param level: уровень минимального логирования хэндлера
+        :param format_: формат записи логов, по умолчанию _std_format
+        :param stream: поток вывода, по умолчанию - консоль"""
         message_handler = logging.StreamHandler(stream)
         message_handler.setLevel(level)
         message_handler.setFormatter(logging.Formatter(format_))
-        self._logger.addHandler(message_handler)
+        self.logger.addHandler(message_handler)
 
 
-def log_message(_type: str, _id: str, _message: str, _status: str):
+def log_message(_type: str, _id: str, _message: str, _status: str) -> str:
     mess_dict = {
         "type": _type,
         "user_id": _id,
@@ -55,60 +60,48 @@ def log_message(_type: str, _id: str, _message: str, _status: str):
 
 def mess_start():
     global message
-    if mess_in_file:
+    if log_out == 'file':
         message.add_file_handler(message_file_info, INFO)
         message.add_file_handler(message_file_warning, WARNING)
-    else:
+    elif log_out == 'console':
         message.add_console_handler(INFO)
+
     return message
-
-
-def start():
-    global message
-    message = Logger('message', INFO)
-    mess_start()
 
 
 def mess_send_info(text: str, id):
     global message
     text = log_message('output', id, text, 'success')
-    message._logger.info(text)
+    message.logger.info(text)
 
 
 def mess_send_warning(text: str, id):
     global message
     text = log_message('output', id, text, 'success')
-    message._logger.warning(text)
+    message.logger.warning(text)
 
 
 def mess_parse_info(text: str, id):
     global message
     text = log_message('input', id, text, 'success')
-    message._logger.info(text)
+    message.logger.info(text)
 
 
 def mess_parse_warning(text: str, id):
     global message
     text = log_message('input', id, text, 'success')
-    message._logger.warning(text)
+    message.logger.warning(text)
 
-# notice = logging.getLogger('notice')
-#
-#
-# notice.setLevel(logging.INFO)
-#
-# message_handlers = []
-# message_handlers.append(logging.FileHandler(message_file_info))
-# message_handlers[0].setLevel(logging.INFO)
-# message_handlers.append(logging.FileHandler(message_file_warning))
-# message_handlers[1].setLevel(logging.WARNING)
-#
-# notice_handler = logging.FileHandler(notice_file)
-# notice_handler.setLevel(logging.INFO)
-#     notice_handler.setFormatter(logging.Formatter(_format))
-#
-# for handler in message_handlers:
-#     message.addHandler(handler)
-#     handler.setFormatter(logging.Formatter(_format))
-# notice.addHandler(notice_handler)
-# notice_handler.setFormatter(logging.Formatter(_format))
+
+def start():
+    global message, log_out, message_file_info, message_file_warning
+
+    with open('config.json', 'r') as config_file:
+        configs = json.load(config_file)
+        log_out = configs['logger_output']
+        if log_out == 'file':
+            message_file_info = configs['message_info']
+            message_file_warning = configs['message_warning']
+
+    message = Logger('message', INFO)
+    mess_start()
