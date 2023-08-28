8bc4f50d1143576f8eda70ac0e85e7960fe84a6a
XMA <xma4602@gmail.com>
2023-08-28 17:22:05 +0400

    Очищен код в data_manager.py

--- System/data_manager.py
+++ System/data_manager.py
@@ -5,19 +5,21 @@ from System.units.notice import Notice
 
 
 def start():
-    global config
-    with open('config.json', 'r') as file:
-        config = json.load(file)
+    pass
 
 
-def dump(file, *args):
-    json.dump(args, file, indent=4, ensure_ascii=False)
+def save(file_name, *args):
+    with open(file_name, 'w') as file:
+        json.dump(args, file, indent=4, ensure_ascii=False)
+
+
+def load(str: file_name):
+    with open(file_name, 'r') as file:
+        return json.load(file)
 
 
 def is_council(id: str):
-    global users
-    with open(config['council'], 'r') as file:
-        users = json.load(file)
+    users = load(config['council'])
     return id in users.keys()
 
 
@@ -26,22 +28,18 @@ def store_event(event):
     Сохраняет событие в файл
     :param event: объект события
     """
-    events = []
-    with open(config['events'], 'r') as file:
-        events = json.load(file)
-        if len(events) == 0:
-            event = event.to_dict()
-            events.append(event)
-        else:
-            for index in range(len(events)):
-                ev = TimeEvent.get_datetime(events[index])
-                if event.time < ev:
-                    event = event.to_dict()
-                    events.insert(index, event)
-                    break
-
-    with open(config['events'], 'w') as file:
-        dump(file, events)
+    events = load(config['events'])
+    if len(events) == 0:
+        event = event.to_dict()
+        events.append(event)
+    else:
+        for index in range(len(events)):
+            ev = TimeEvent.get_datetime(events[index])
+            if event.time < ev:
+                event = event.to_dict()
+                events.insert(index, event)
+                break
+    save(config['events'], events)
 
 
 def get_nearest_event(old_event=None):
@@ -49,17 +47,13 @@ def get_nearest_event(old_event=None):
     Возвращает ближайшее событие
     :params old_event: исполнившееся событие
     """
-    events = []
-    with open(config['events'], 'r') as file:
-        events = json.load(file)  # считывает список событий из файла
-
+    events = load(config['events'])  # считывает список событий из файла
     if len(events) == 0:  # если событий нет, то возвращает None
         return None
     else:
         if old_event is not None:  # если передано старое событие, то удаляет его из списка
             events.pop(0)
-            with open(config['events'], 'w') as file:
-                dump(file, events)  # записывает измененный список обратно в файл
+            save(config['events'], events)  # записывает измененный список обратно в файл
         if len(events) == 0:
             return None
         nearest_event = events[0]  # получаем ближайшее событие
@@ -73,9 +67,7 @@ def check_fired_events():
     Используется единожды при запуске, возвращает ближайшее не просроченное событие
     """
 
-    events = []
-    with open(config['events'], 'r') as file:  # считываем список событий
-        events = json.load(file)
+    events = load(config['events'])  # считываем список событий
 
     fired_events = []  # список просроченных событий
     event = events[0]  # получаем ближайшее событие
@@ -89,25 +81,19 @@ def check_fired_events():
         event = events[0]
         delta = datetime.now() - TimeEvent.get_datetime(event)
 
-    with open(config['events'], 'w') as file:  # записываем в файл список запланированных событий без просрочек
-        dump(file, events)
+    save(config['events'], events) # записываем в файл список запланированных событий без просрочек
 
-    load_fired_events = []  # список из файла просроченных событий
     if len(fired_events) != 0:
-        with open(config['fired'], 'r') as file:
-            load_fired_events = json.load(file)  # загружаем список просрочек из файла
-        with open(config['fired'], 'w') as file:
-            load_fired_events += fired_events  # добавляем в список новые просрочки
-            json.dump(load_fired_events, file, indent=4)  # загружаем все обратно в файл
+        load_fired_events = load(config['fired']) # загружаем список просрочек из файла
+        load_fired_events += fired_events  # добавляем в список новые просрочки
+        save(config['fired'], load_fired_events) # загружаем все обратно в файл
 
 
 def get_fired_events():
     """
     Возвращает список просроченных событий
     """
-    fired_events = []
-    with open(config['fired'], 'r') as file:
-        fired_events = json.load(file)
+    fired_events = load(config['fired'])
 
     if len(fired_events) == 0:
         return None
@@ -147,12 +133,6 @@ def names_to_id(users_surnames: list):
             if users_surnames[i] == name:
                 users_surnames[i] = id
     return users_surnames
-
-
-def get_vk_group_data():
-    with open(config['vk_keys'], 'r') as file:
-        data = json.load(file)
-    return data
 
 
 
