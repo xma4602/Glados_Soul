2271471d238a6cc05947dc0c387b2b77dff1ad09
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-09-05 16:30:48 +0400

    Обновлен логгер

--- System/modules/logger.py
+++ System/modules/logger.py
@@ -1,78 +1,32 @@
-# -*- coding: utf-8 -*-
 import logging
-from logging import INFO, ERROR
+from System import config_manager
 import sys
 
-from System import config_manager
 
-
-class Logger:
-    """
-    Класс для удобного создания объекта лога
-    """
+def start():
+    handlers = []
+    print('Запуск логгера')
+    # формат вывовда логов
     log_format = '%(asctime)s - %(levelname)s - %(message)s - %(args)s'
 
-    def __init__(self, name: str, level, log_out: str, file_path: str):
-        self.logger = logging.getLogger(name)  # создание логгера
-        self.logger.setLevel(level)  # задание минимального уровня логирования
-
-        if log_out == 'file':
-            self.add_file_handler(INFO, file_path)
-            self.add_console_handler(ERROR)
-        elif log_out == 'console':
-            self.add_console_handler(INFO)
-        else:
-            raise ValueError('Unexpected value for log out')
-
-    def add_file_handler(self, level: int, file: str, ) -> None:
-        """Добавляет в логгер хэндлер для записи логов в файл
-        :param file: путь к файлу для записи логов
-        :param level: уровень минимального логирования хэндлера
-        """
-        handler = logging.FileHandler(file)
-        handler.setLevel(level)
-        handler.setFormatter(logging.Formatter(self.log_format))
-
-        self.logger.addHandler(handler)
-
-    def add_console_handler(self, level, stream=sys.stdout) -> None:
-        """Добавляет в логгер хэндлер для записи логов в консоль
-        :param level: уровень минимального логирования хэндлера
-        :param stream: поток вывода, по умолчанию - консоль"""
-        handler = logging.StreamHandler(stream)
-        handler.setLevel(level)
-        handler.setFormatter(logging.Formatter(self.log_format))
-
-        self.logger.addHandler(handler)
-
-
-print('Запуск модуля logger')
-log = Logger('main_logger',
-             config_manager.log_level(),
-             config_manager.log_out(),
-             config_manager.log_file()
-             )
-
-
-def info(massage: str, **data) -> None:
-    global log
-    if len(data) == 0:
-        log.logger.info(massage)
+    output = config_manager.log_out()
+    if output == 'file':
+        file_handler = logging.FileHandler(config_manager.log_file())
+        file_handler.setLevel(logging.INFO)
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(logging.ERROR)
+        handlers = [file_handler, stream_handler]
+    elif output == 'console':
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(logging.INFO)
+        handlers = [stream_handler]
     else:
-        log.logger.info(massage, data)
+        raise ValueError('Unexpected value for log out')
+
+    logging.basicConfig(
+        handlers=handlers,
+        level=config_manager.log_level(),
+        format=log_format
+    )
 
 
-def warning(massage: str, **data) -> None:
-    global log
-    if len(data) == 0:
-        log.logger.warning(massage)
-    else:
-        log.logger.warning(massage, data)
-
-
-def error(massage: str, **data) -> None:
-    global log
-    if len(data) == 0:
-        log.logger.error(massage)
-    else:
-        log.logger.error(massage, data)
---
+++ System/modules/logger_old.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import logging
+from logging import INFO, ERROR
+import sys
+
+from System import config_manager
+
+
+class Logger:
+    """
+    Класс для удобного создания объекта лога
+    """
+    log_format = '%(asctime)s - %(levelname)s - %(message)s - %(args)s'
+
+    def __init__(self, name: str, level, log_out: str, file_path: str):
+        self.logger = logging.getLogger(name)  # создание логгера
+        self.logger.setLevel(level)  # задание минимального уровня логирования
+
+        if log_out == 'file':
+            self.add_file_handler(INFO, file_path)
+            self.add_console_handler(ERROR)
+        elif log_out == 'console':
+            self.add_console_handler(INFO)
+        else:
+            raise ValueError('Unexpected value for log out')
+
+    def add_file_handler(self, level: int, file: str, ) -> None:
+        """Добавляет в логгер хэндлер для записи логов в файл
+        :param file: путь к файлу для записи логов
+        :param level: уровень минимального логирования хэндлера
+        """
+        handler = logging.FileHandler(file)
+        handler.setLevel(level)
+        handler.setFormatter(logging.Formatter(self.log_format))
+
+        self.logger.addHandler(handler)
+
+    def add_console_handler(self, level, stream=sys.stdout) -> None:
+        """Добавляет в логгер хэндлер для записи логов в консоль
+        :param level: уровень минимального логирования хэндлера
+        :param stream: поток вывода, по умолчанию - консоль"""
+        handler = logging.StreamHandler(stream)
+        handler.setLevel(level)
+        handler.setFormatter(logging.Formatter(self.log_format))
+
+        self.logger.addHandler(handler)
+
+
+print('Запуск модуля logger')
+log = Logger('main_logger',
+             config_manager.log_level(),
+             config_manager.log_out(),
+             config_manager.log_file()
+             )
+
+
+def info(massage: str, **data) -> None:
+    global log
+    if len(data) == 0:
+        log.logger.info(massage)
+    else:
+        log.logger.info(massage, data)
+
+
+def warning(massage: str, **data) -> None:
+    global log
+    if len(data) == 0:
+        log.logger.warning(massage)
+    else:
+        log.logger.warning(massage, data)
+
+
+def error(massage: str, **data) -> None:
+    global log
+    if len(data) == 0:
+        log.logger.error(massage)
+    else:
+        log.logger.error(massage, data)
