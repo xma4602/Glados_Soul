23d3d123eecd9f0f219156fe375ffdcfe3ce6bbb
Sapdotten <semenova.a.p.04@gmail.com>
2023-11-12 20:34:46 +0400

    Создание бд никнеймов

--- System/data_manager_sql.py
+++ System/data_manager_sql.py
@@ -3,6 +3,9 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2 import Error
 import logging
 import psycopg2._psycopg as ptyping
+from modules import logger
+
+logger.start()
 
 
 def _make_connection_to_db() -> ptyping.connection:
@@ -13,38 +16,38 @@ def _make_connection_to_db() -> ptyping.connection:
                             database='glados_db')
 
 
-def _close_connection(connection: ptyping.connection, cursor: ptyping.cursor):
-    if connection:
+def _close_connection(conn: ptyping.connection, cursor: ptyping.cursor):
+    if conn:
         cursor.close()
-        connection.close()
+        conn.close()
         logging.info("Connection to database is closed")
 
 
 def create_db():
-    connection = None
+    conn = None
     cursor = None
     try:
-        connection = psycopg2.connect(user='postgres',
-                                      password='1234',
-                                      host='localhost',
-                                      port='5432')
-        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        cursor = connection.cursor()
+        conn = psycopg2.connect(user='postgres',
+                                password='1234',
+                                host='localhost',
+                                port='5432')
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = conn.cursor()
         sql_create_database = 'CREATE DATABASE glados_db'
         cursor.execute(sql_create_database)
         logging.info("Create a database glados_db")
     except Error as error:
         logging.error("Error for creating a database", {'error': error})
     finally:
-        _close_connection(connection, cursor)
+        _close_connection(conn, cursor)
 
 
 def create_table_users():
-    connection = None
+    conn = None
     cursor = None
     try:
-        connection = _make_connection_to_db()
-        cursor = connection.cursor()
+        conn = _make_connection_to_db()
+        cursor = conn.cursor()
         create_table_query = "CREATE TABLE users(" \
                              "id INT PRIMARY KEY NOT NULL, " \
                              "phone VARCHAR(11), " \
@@ -53,9 +56,32 @@ def create_table_users():
                              "patroname VARCHAR(255) ," \
                              "university_id VARCHAR(8));"
         cursor.execute(create_table_query)
-        connection.commit()
+        conn.commit()
         logging.info("create a table users in database")
     except (Exception, Error) as error:
         logging.error("Error for creating a table users in database", {'error': error})
     finally:
-        _close_connection(connection, cursor)
+        _close_connection(conn, cursor)
+
+
+def create_table_nicknames():
+    conn = None
+    cursor = None
+    try:
+        conn = _make_connection_to_db()
+        cursor = conn.cursor()
+        create_table_query = "CREATE TABLE nicknames(" \
+                             "owner_id INT NOT NULL REFERENCES users(id), " \
+                             "named_id INT NOT NULL REFERENCES users(id), " \
+                             "nickname VARCHAR(255));"
+        cursor.execute(create_table_query)
+        conn.commit()
+        logging.info('Create a table nicknames in database')
+    except (Exception, Error) as error:
+        logging.error('Error by creating a table nicknames in database', {"error": error})
+    finally:
+        _close_connection(conn, cursor)
+
+
+create_table_users()
+create_table_nicknames()
