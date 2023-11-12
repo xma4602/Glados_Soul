2a990a6d8e0125cdde41ffd5ce50edef1e36cddb
Sapdotten <semenova.a.p.04@gmail.com>
2023-11-12 20:07:55 +0400

    Заменила принты на логи, добавила хинты

--- System/data_manager_sql.py
+++ System/data_manager_sql.py
@@ -1,9 +1,11 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2 import Error
+import logging
+import psycopg2._psycopg as ptyping
 
 
-def make_connection_to_db() -> psycopg2.connection:
+def _make_connection_to_db() -> ptyping.connection:
     return psycopg2.connect(user='postgres',
                             password='1234',
                             host='localhost',
@@ -11,7 +13,16 @@ def make_connection_to_db() -> psycopg2.connection:
                             database='glados_db')
 
 
+def _close_connection(connection: ptyping.connection, cursor: ptyping.cursor):
+    if connection:
+        cursor.close()
+        connection.close()
+        logging.info("Connection to database is closed")
+
+
 def create_db():
+    connection = None
+    cursor = None
     try:
         connection = psycopg2.connect(user='postgres',
                                       password='1234',
@@ -21,19 +32,18 @@ def create_db():
         cursor = connection.cursor()
         sql_create_database = 'CREATE DATABASE glados_db'
         cursor.execute(sql_create_database)
-    except (Exception, Error) as error:
-        print('Error for creating db')
-        print(error)
+        logging.info("Create a database glados_db")
+    except Error as error:
+        logging.error("Error for creating a database", {'error': error})
     finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print('Conn is closed')
+        _close_connection(connection, cursor)
 
 
 def create_table_users():
+    connection = None
+    cursor = None
     try:
-        connection = make_connection_to_db()
+        connection = _make_connection_to_db()
         cursor = connection.cursor()
         create_table_query = "CREATE TABLE users(" \
                              "id INT PRIMARY KEY NOT NULL, " \
@@ -44,18 +54,8 @@ def create_table_users():
                              "university_id VARCHAR(8));"
         cursor.execute(create_table_query)
         connection.commit()
-        print('table created')
+        logging.info("create a table users in database")
     except (Exception, Error) as error:
-        print('Error: ', error)
+        logging.error("Error for creating a table users in database", {'error': error})
     finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print('Connection is closed')
-
-# id: uuid
-# phone:string
-# name: string
-# surname: string
-# patroname: string
-# group_id: string
+        _close_connection(connection, cursor)
